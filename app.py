6c72cead589333fa87ff7350a610b944c9581a30
kensan23 <9150185+kensan23@users.noreply.github.com>
2020-05-02 04:36:56 +0000

    add smbus dependancy

--- app.py
+++ app.py
@@ -5,21 +5,21 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from dotenv import load_dotenv
 from sensor_data import sensor_data
+load_dotenv()
 
-try:
+def post_data(sensordata):
     BASEDIR = os.path.abspath(os.path.dirname(__file__))
-    load_dotenv()
-    #token = os.getenv("AWSSNSTOPICARN")
-    data = sensor_data()
-    #sns = boto3.client('sns')
-
+    token = os.getenv("AWSSNSTOPICARN")
+    sns = boto3.client('sns')
     # response = sns.publish(
     #     TopicArn=token,   
     #     Message=json.dumps(data.get_data("study")),   
     # )
-    print(data.get_data("study"))
+
+try:
+    data = sensor_data()
     sched = BlockingScheduler()
-    sched.add_job(sensor_data, 'cron', second='*/5')
+    sched.add_job(post_data, 'cron', minute='*/1', args=[data])
     sched.start()
 
 except Exception as e:

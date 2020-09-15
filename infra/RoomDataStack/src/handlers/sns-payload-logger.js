8abb338c04fd101f9a4429519cbe3d5a6afcd2bb
kensan <9150185+kensan23@users.noreply.github.com>
2020-09-15 21:54:57 +1000

    Update to parse JSON
    parse raw message before sending

--- infra/RoomDataStack/src/handlers/sns-payload-logger.js
+++ infra/RoomDataStack/src/handlers/sns-payload-logger.js
@@ -9,8 +9,8 @@ exports.snsPayloadLoggerHandler = async (event, context) => {
   // All log statements are written to CloudWatch by default. For more information, see
   // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
   const tableName = process.env.DYNAMO_DB_NAME;
-  const message = event.Records[0].Sns.Message;
-
+  const messageRaw = event.Records[0].Sns.Message;
+  const message = JSON.parse(messageRaw);
   const params = {
     TableName: tableName,
     Item: {
